5e4b1d1abfdd593d21a6caf83e5a9810dc6f9896
Eugene Tomes <eugenetomesdev@gmail.com>
2024-05-28 23:14:15 +0200

    Add Logging

--- src/OnsTrou.App/Startup.cs
+++ src/OnsTrou.App/Startup.cs
@@ -23,12 +23,14 @@ public class Startup
     }
 
     // This method gets called by the runtime. Use this method to configure the HTTP request pipeline
-    public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+    public void Configure(IApplicationBuilder app, IWebHostEnvironment env, ILoggerFactory loggerFactory)
     {
         if (env.IsDevelopment())
         {
             app.UseDeveloperExceptionPage();
         }
+        var loggerOptions = new LambdaLoggerOptions(Configuration);
+        loggerFactory.AddLambdaLogger(loggerOptions);
 
 
         app.UseSwagger();
